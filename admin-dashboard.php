f3dc177f14545b2455c73e5f9ec6a4bad0a61aa0
Hennn112 <mohamadaristian10@gmail.com>
2023-09-17 17:57:24 +0700

    Hendra

---
+++ admin-dashboard.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/Wikrama-logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="style/admin-page.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://fonts.googleapis.com/css?family=Epilogue' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300&display=swap" rel="stylesheet">
+    <title>GDS Wikrama</title>
+</head>
+<body>
+    <div class="sidebar">
+        <img src="img/Ellipse 1.svg" alt="">
+        <h1>Admin Wikrama</h1>
+        <a href="#services"><img src="img/layouts (1) 1.svg" alt=""> Dashboard</a>
+        <a href="#clients"><img src="img/user (1) 1.svg" alt=""> Data Siswa</a>
+        <a href="#contact"><img src="img/document 1.svg" alt=""> Data Laporan</a>
+        <a href="#add"><img src="img/add-user 1.svg" alt=""> Tambah Siswa</a>
+
+    </div>
+    <div class="content">
+        <div class="topnav">
+            <img src="img/Wikrama.png" alt="">
+            <p class="active" href="#home">GDS <span>Wikrama</span></p>
+        </div>
+        <div class="clearfix"></div>  
+        <h1>Data Siswa</h1>
+        <hr>    
+        <h6>Tambah Data Siswa</h6>
+        
+        <table class="table" border="1">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Rayon</th>
+                <th>NIS</th>
+                <th>Rombel</th>
+                <th>Aksi</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>John Doe</td>
+                <td>R001</td>
+                <td>123456</td>
+                <td>10A</td>
+                <td><a href="#">Edit </a><a href="">Hapus</a></td>
+            </tr>
+        </table>
+    </div>
+    
+</body>
+</html>
